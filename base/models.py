7e59d309ecf9e7014b7f58bb9179cce078223139
unknown <akbiyik.ilker7@gmail.com>
2022-08-23 22:05:40 +0200

    basic models

---
+++ base/models.py
@@ -0,0 +1,26 @@
+from turtle import right
+from django.db import models
+
+# Create your models here.
+class Test(models.Model):
+    name = models.CharField(max_length=60)
+
+    def __str__(self) -> str:
+        return str(self.name)
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+    test = models.ForeignKey(Test, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.text)
+
+
+class Answer(models.Model):
+    text = models.CharField(max_length=255)
+    right = models.BooleanField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return str(self.text)
