57c5efbecd17e565645336a0deddd0c4d76b31cb
unknown <akbiyik.ilker7@gmail.com>
2022-08-28 18:58:26 +0200

    add user methods

--- base/models.py
+++ base/models.py
@@ -1,4 +1,13 @@
+import datetime
+import email
+
 from django.db import models
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
+from django.utils import timezone
+
 
 # Create your models here.
 class Test(models.Model):
@@ -23,3 +32,83 @@ class Answer(models.Model):
 
     def __str__(self) -> str:
         return str(self.text)
+
+
+class MyUserManager(BaseUserManager):
+    
+    def create_user(self, email, date_of_birth, password=None):
+        if not email:
+            raise ValueError('User must have an email address')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            date_of_birth=date_of_birth
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, date_of_birth, password=None):
+        user = self.create_user(
+            email,
+            password=password,
+            date_of_birth=date_of_birth
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+
+class MyUser(AbstractBaseUser):
+
+    email = models.EmailField(
+        max_length=255,
+        unique=True
+    )
+    date_of_birth = models.DateField()
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    credits = models.PositiveIntegerField(default=100)
+    linkedin_token = models.TextField(blank=True, default='')
+    expiry_date = models.DateTimeField(null=True, blank=True)
+    objects = MyUserManager()
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['date_of_birth']
+
+    def __str__(self) -> str:
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        # Return true for now
+        return True
+
+    def has_module_perm(self, perm, obj=None):
+        # Return true for now
+        return True
+
+    # COPIED AFTER THIS POINT CHECK OUT LATER 
+    @property
+    def is_staff(self):
+        "Is the user a member of staff?"
+        # Simplest possible answer: All admins are staff
+        return self.is_admin
+
+    @property
+    def is_out_of_credits(self):
+        "Is the user out  of credits?"
+        return self.credits > 0
+
+    @property
+    def has_sufficient_credits(self, cost):
+        return self.credits - cost >= 0
+
+    @property
+    def linkedin_signed_in(self):
+
+        return bool(self.linkedin_token) and self.expiry_date > timezone.now()
+
+        
+
